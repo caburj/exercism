d7598cb2cf6e8b3508ec66862bcccd7376eaea58
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-19 22:00:49 +0200

    Solution to elm/bob.

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,6 +1,41 @@
 module Bob exposing (hey)
 
+import Regex exposing (HowMany(..), regex, replace)
+
+
+getAlphaNumQuestion =
+    replace All (regex "[^(a-zA-Z0-9)\\s^(\\?)]") (\_ -> "") << String.trim
+
+
+getAlpha =
+    replace All (regex "[0-9\\s\\?]") (\_ -> "")
+
+
+isQuestion =
+    String.endsWith "?"
+
+
+isShout remark =
+    if (String.isEmpty << getAlpha) remark then
+        False
+    else
+        String.toUpper remark == remark
+
 
 hey : String -> String
 hey remark =
-    Debug.crash "Please implement this function"
+    let
+        remark_ =
+            getAlphaNumQuestion remark
+    in
+    if isQuestion remark_ then
+        if isShout remark_ then
+            "Calm down, I know what I'm doing!"
+        else
+            "Sure."
+    else if String.isEmpty remark_ then
+        "Fine. Be that way!"
+    else if isShout remark_ then
+        "Whoa, chill out!"
+    else
+        "Whatever."

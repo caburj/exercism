f9747490930d7bcce728d4a76f9adad2f391ba57
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-20 00:37:04 +0200

    Solution to elm/collatz-conjecture.

--- elm/collatz-conjecture/CollatzConjecture.elm
+++ elm/collatz-conjecture/CollatzConjecture.elm
@@ -3,4 +3,22 @@ module CollatzConjecture exposing (collatz)
 
 collatz : Int -> Result String Int
 collatz start =
-    Debug.crash "Please implement this function"
+    if start <= 0 then
+        Err "Only positive numbers are allowed"
+    else
+        Ok <| collatz_ 0 start
+
+
+collatz_ : Int -> Int -> Int
+collatz_ count start =
+    if start == 1 then
+        count
+    else if isEven start then
+        collatz_ (count + 1) (start // 2)
+    else
+        collatz_ (count + 1) (3 * start + 1)
+
+
+isEven : Int -> Bool
+isEven n =
+    n % 2 == 0

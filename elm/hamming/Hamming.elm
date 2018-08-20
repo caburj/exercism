cd4719d76ec0460976d844c2882be1eeffa3f91e
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-20 23:40:43 +0200

    Solution to elm/hamming.

--- elm/hamming/Hamming.elm
+++ elm/hamming/Hamming.elm
@@ -3,4 +3,26 @@ module Hamming exposing (distance)
 
 distance : String -> String -> Result String Int
 distance left right =
-    Debug.crash "Please implement this function"
+    if String.length left /= String.length right then
+        Err "left and right strands must be of equal length"
+    else
+        let
+            leftList =
+                String.toList left
+
+            rightList =
+                String.toList right
+        in
+        Ok <| List.foldl incrementDistance 0 (zip leftList rightList)
+
+
+zip : List a -> List b -> List ( a, b )
+zip =
+    List.map2 (,)
+
+
+incrementDistance ( l, r ) d =
+    if l /= r then
+        d + 1
+    else
+        d

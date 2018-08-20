3d9ad87d2e1806b0ba7fbb9a81f2126ad81cb673
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-20 22:03:25 +0200

    Solution to elm/grains.

--- elm/grains/Grains.elm
+++ elm/grains/Grains.elm
@@ -3,4 +3,15 @@ module Grains exposing (square)
 
 square : Int -> Maybe Int
 square n =
-    Debug.crash "Please implement this function"
+    if n < 1 then
+        Nothing
+    else
+        Just << exp 2 <| (n - 1)
+
+
+exp : Int -> Int -> Int
+exp a n =
+    if n == 0 then
+        1
+    else
+        a * exp a (n - 1)

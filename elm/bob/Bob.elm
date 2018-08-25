c0eddcc7fe0464190a907673fb431653dbdf374d
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-25 23:27:39 +0200

    Improvement of the bob exercise based on JoelQ's suggestions.
    - Type signatures
    - Avoid conditional expressions to return bool
    - Can be solved without regex

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,41 +1,59 @@
 module Bob exposing (hey)
 
-import Regex exposing (HowMany(..), regex, replace)
+import Char
 
 
-getAlphaNumQuestion =
-    replace All (regex "[^(a-zA-Z0-9)\\s^(\\?)]") (\_ -> "") << String.trim
+
+-- import Regex exposing (HowMany(..), regex, replace)
+-- getAlphaNumQuestion : String -> String
+-- getAlphaNumQuestion =
+--     replace All (regex "[^(a-zA-Z0-9)\\s^(\\?)]") (\_ -> "") << String.trim
+-- getAlpha : String -> String
+-- getAlpha =
+--     replace All (regex "[0-9\\s\\?]") (\_ -> "")
 
 
-getAlpha =
-    replace All (regex "[0-9\\s\\?]") (\_ -> "")
+onlyAlphabets : String -> String
+onlyAlphabets =
+    String.filter isAlphabet
 
 
+{-| I can't find a built-in function that checks whether a char is an alphabet,
+so I just implemented a simple check, might work in most cases, meh..
+-}
+isAlphabet : Char -> Bool
+isAlphabet c =
+    not (Char.toLower c == c && Char.toUpper c == c)
+
+
+isQuestion : String -> Bool
 isQuestion =
     String.endsWith "?"
 
 
+isShout : String -> Bool
 isShout remark =
-    if (String.isEmpty << getAlpha) remark then
-        False
-    else
-        String.toUpper remark == remark
+    String.toUpper remark == remark && not ((String.isEmpty << onlyAlphabets) remark)
 
 
 hey : String -> String
 hey remark =
     let
         remark_ =
-            getAlphaNumQuestion remark
+            String.trim remark
     in
     if isQuestion remark_ then
         if isShout remark_ then
             "Calm down, I know what I'm doing!"
+
         else
             "Sure."
+
     else if String.isEmpty remark_ then
         "Fine. Be that way!"
+
     else if isShout remark_ then
         "Whoa, chill out!"
+
     else
         "Whatever."

73577567363f10f8fd979d12c1db0f38173e4578
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-29 00:51:22 +0200

    Solution to elm/run-length-encoding.

--- elm/run-length-encoding/RunLengthEncoding.elm
+++ elm/run-length-encoding/RunLengthEncoding.elm
@@ -1,15 +1,69 @@
 module RunLengthEncoding exposing (decode, encode, version)
 
+import Char
+
 
 version =
     2
 
 
 encode : String -> String
-encode string =
-    Debug.crash "Please implement this function"
+encode =
+    convert "" << String.foldr updateCounter []
+
+
+updateCounter : Char -> List ( Char, Int ) -> List ( Char, Int )
+updateCounter c pairs =
+    case pairs of
+        [] ->
+            ( c, 1 ) :: []
+
+        ( oldC, count ) :: xs ->
+            if oldC == c then
+                ( oldC, (+) 1 count ) :: xs
+
+            else
+                ( c, 1 ) :: ( oldC, count ) :: xs
+
+
+convert : String -> List ( Char, Int ) -> String
+convert string pairs =
+    case pairs of
+        [] ->
+            string
+
+        ( c, count ) :: xs ->
+            if count == 1 then
+                convert (string ++ String.fromChar c) xs
+
+            else
+                convert (string ++ toString count ++ String.fromChar c) xs
 
 
 decode : String -> String
-decode string =
-    Debug.crash "Please implement this function"
+decode =
+    decode_ "" [] << String.toList
+
+
+decode_ : String -> List Char -> List Char -> String
+decode_ result digits charList =
+    case charList of
+        [] ->
+            result
+
+        x :: xs ->
+            if Char.isDigit x then
+                decode_ result (digits ++ [ x ]) xs
+
+            else
+                decode_ (result ++ (String.fromList << List.repeat (toInt digits)) x) [] xs
+
+
+toInt : List Char -> Int
+toInt digits =
+    case String.toInt << String.fromList <| digits of
+        Ok val ->
+            val
+
+        Err _ ->
+            1

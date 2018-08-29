b10419e93ce62606a6bda0c0011ff71023966d04
Joseph Caburnay <caburnay.joseph@gmail.com>
2018-08-29 22:39:54 +0200

    Solution to elm/sublist. But a very ugly one.

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -15,4 +15,63 @@ type ListComparison
 
 sublist : List a -> List a -> ListComparison
 sublist alist blist =
-    Debug.crash "Please implement this function"
+    case ( alist, blist ) of
+        ( [], [] ) ->
+            Equal
+
+        ( [], _ ) ->
+            Sublist
+
+        ( _, [] ) ->
+            Superlist
+
+        ( _, _ ) ->
+            sublist_ alist blist
+
+
+sublist_ : List a -> List a -> ListComparison
+sublist_ alist blist =
+    let
+        alen =
+            List.length alist
+
+        blen =
+            List.length blist
+    in
+    if alen == blen then
+        iff (List.all identity <| List.map2 (==) alist blist) Equal Unequal
+
+    else if alen < blen then
+        iff (subset alist blist) Sublist Unequal
+
+    else
+        iff (subset blist alist) Superlist Unequal
+
+
+subset : List a -> List a -> Bool
+subset slist blist =
+    List.foldl (equalToList slist) False (listOf (List.length slist) blist)
+
+
+equalToList : List a -> List a -> Bool -> Bool
+equalToList slist list val =
+    val || (List.all identity <| List.map2 (==) slist list)
+
+
+listOf : Int -> List a -> List (List a)
+listOf n blist =
+    List.map (flip slice blist) (List.map2 (,) (List.range 0 (List.length blist - n)) (List.range n (List.length blist)))
+
+
+slice : ( Int, Int ) -> List a -> List a
+slice ( i, j ) =
+    List.drop i << List.take (j - i + 1)
+
+
+iff : Bool -> a -> a -> a
+iff pred a b =
+    if pred then
+        a
+
+    else
+        b
